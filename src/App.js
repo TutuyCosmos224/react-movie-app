1260c9aa1252a461a39010238752beba1a95ce2c
Hansen Emanuel <hansenemanuel@hotmail.com>
2021-10-25 22:44:23 +0700

    add search query and price display

--- src/App.js
+++ src/App.js
@@ -10,20 +10,42 @@ const SEARCH_API = "https://api.themoviedb.org/3/search/movie?api_key=d145bc28c0
 
 function App() {
   const [movies, setMovies] = useState([]);
+  const [searchTerm, setSearchTerm] = useState([]);
 
   useEffect( () =>{
-    fetch(DISCOVER_API)
+    getMovies(DISCOVER_API)
+  }, [])
+
+  const getMovies=(API) => {
+    fetch(API)
       .then((res) => res.json())
       .then((data) => {
         console.log(data)
         setMovies(data.results);
       })
-  }, [])
+  }
+
+  const handleOnSubmit = (e) => {
+    e.preventDefault();
+
+    if (searchTerm){
+      getMovies(SEARCH_API+searchTerm)
+      setSearchTerm('');
+
+  }
+  };
+
+  const handleOnChange = (e) => {
+    setSearchTerm(e.target.value);
+  }
 
   return(
     <>
       <header>
-        <input className = "search" type="search" placeholder="Search" />
+        <a href="/"><h2>StreamFlix</h2></a>
+        <form onSubmit={handleOnSubmit}>
+          <input className = "search" type="search" placeholder="Search" value={searchTerm} onChange = {handleOnChange} />
+        </form>
       </header>
 
         <div className = "movie-container">
