1eabce24354e9a3cddc2e496f5cde465b48694de
Hansen Emanuel <hansenemanuel@hotmail.com>
2021-10-25 20:37:20 +0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect, useState} from 'react';
+
+import Movie from './components/Movie';
+
+const DISCOVER_API = "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&region=ID&api_key=d145bc28c096d7d6a36862a50196d769&page=1"
+
+const IMG_API = "https://image.tmdb.org/t/p/w1280"
+
+const SEARCH_API = "https://api.themoviedb.org/3/search/movie?api_key=d145bc28c096d7d6a36862a50196d769&query="
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+  const [movies, setMovies] = useState([]);
+
+  useEffect( () =>{
+    fetch(DISCOVER_API)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data)
+        setMovies(data.results);
+      })
+  }, [])
+
+  return(
+    <>
+      <header>
+        <input className = "search" type="search" placeholder="Search" />
       </header>
-    </div>
+
+        <div className = "movie-container">
+          {movies.length > 0 && movies.map (movie => ( <Movie key = {movie.id} {...movie}/>))}
+        </div>
+    </>
   );
 }
 
