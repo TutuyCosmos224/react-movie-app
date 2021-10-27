25122c15fd4c13887acb6bb0405b2becbe624205
Hansen Emanuel <hansenemanuel@hotmail.com>
2021-10-27 14:59:15 +0700

    add pagination

--- src/App.js
+++ src/App.js
@@ -2,18 +2,23 @@ import React, {useEffect, useState} from 'react';
 
 import Movie from './components/Movie';
 
-const DISCOVER_API = "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&region=ID&api_key=d145bc28c096d7d6a36862a50196d769&page=1"
+import ReactPaginate from 'react-paginate';
+
+const DISCOVER_API = "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&region=ID&api_key=d145bc28c096d7d6a36862a50196d769&page="
 
 const IMG_API = "https://image.tmdb.org/t/p/w1280"
 
 const SEARCH_API = "https://api.themoviedb.org/3/search/movie?api_key=d145bc28c096d7d6a36862a50196d769&query="
 
 function App() {
+  
+  
   const [movies, setMovies] = useState([]);
   const [searchTerm, setSearchTerm] = useState([]);
+  const [currentPage] = useState(1)
 
   useEffect( () =>{
-    getMovies(DISCOVER_API)
+    getMovies(DISCOVER_API + currentPage)
   }, [])
 
   const getMovies=(API) => {
@@ -39,6 +44,10 @@ function App() {
     setSearchTerm(e.target.value);
   }
 
+  const handlePageClick = async(data) => {
+    getMovies (DISCOVER_API + (data.selected+1))
+  }
+
   return(
     <>
       <header>
@@ -51,6 +60,25 @@ function App() {
         <div className = "movie-container">
           {movies.length > 0 && movies.map (movie => ( <Movie key = {movie.id} {...movie}/>))}
         </div>
+
+      <ReactPaginate
+      previousLabel = {'previous'}
+      nextLabel = {'next'}
+      breakLabel = {'...'}
+      pageCount = {20}
+      marginPagesDisplayed = {2}
+      onPageChange = {handlePageClick} 
+      containerClassName = {'pagination justify-content-center'}
+      pageClassName = {'page-item'}
+      pageLinkClassName = {'page-link'}  
+      previousClassName = {'page-item'}
+      previousLinkClassName = {'page-link'}
+      nextClassName = {'page-item'}
+      nextLinkClassName = {'page-link'}
+      breakClassName = {'page-item'}
+      breakLinkClassName = {'page-link'}  
+      activeClassName = {'active'} 
+      />
     </>
   );
 }
--- src/components/Movie.js
+++ src/components/Movie.js
@@ -2,18 +2,6 @@ import React from 'react';
 
 const IMG_API = "https://image.tmdb.org/t/p/w1280"
 
-const setPrice = (vote) => {
-    if (vote > 8){
-        return <span>21250</span>;
-    } else if (vote > 6){
-        return <span>16350</span>;
-    } else if (vote > 3){
-        return <span>8250</span>;
-    } else{
-        return <span>3500</span>;
-    }
-}
-
 const Movie = ({title, poster_path, overview, vote_average}) => (
     <div className = "movie">
         <img src={poster_path ? (IMG_API + poster_path) : 'https://images.unsplash.com/photo-1563991655280-cb95c90ca2fb?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1170&q=80'} alt={title} />
@@ -23,13 +11,13 @@ const Movie = ({title, poster_path, overview, vote_average}) => (
                 {
                     (() => {
                         if (vote_average >= 8){
-                            return <span>21250</span>;
+                            return <span>IDR 21250</span>;
                         } else if (vote_average >= 6){
-                            return <span>16350</span>;
+                            return <span>IDR 16350</span>;
                         } else if (vote_average >= 3){
-                            return <span>8250</span>;
+                            return <span>IDR 8250</span>;
                         } else{
-                            return <span>3500</span>;
+                            return <span>IDR 3500</span>;
                         }
                     })()
                 }
